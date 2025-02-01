7e8b38cdffd6098499e706518a7e64b18c3a5741
hyper <hyperskill@hyperskill.org>
2025-02-01 02:03:12 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let stdin = io::stdin();
@@ -7,6 +8,21 @@ fn main() {
         io::stdout().flush().expect("Failed to flush stdout");
         let mut input = String::new();
         stdin.read_line(&mut input).expect("Failed to read line");
-        println!("{}: command not found", input.trim());
+        let trimmed_input = input.trim();
+
+        if trimmed_input.starts_with("exit") {
+            let parts: Vec<&str> = trimmed_input.split_whitespace().collect();
+            if parts.len() == 2 {
+                if let Ok(code) = parts[1].parse::<i32>() {
+                    exit(code);
+                } else {
+                    eprintln!("Invalid exit code: {}", parts[1]);
+                }
+            } else {
+                exit(0);
+            }
+        }
+
+        println!("{}: command not found", trimmed_input);
     }
 }
