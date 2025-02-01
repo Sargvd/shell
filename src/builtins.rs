9bdad11eec5742e4d2717b4bcec1ea4b20165745
hyper <hyperskill@hyperskill.org>
2025-02-01 19:55:54 +0100

    codecrafters submit [skip ci]

--- src/builtins.rs
+++ src/builtins.rs
@@ -4,48 +4,46 @@ use std::process::exit;
 
 pub static BUILTINS: &[&str] = &["exit", "echo", "type", "pwd", "cd"];
 
-pub fn builtin_exit(input: &str) {
-    if input.starts_with("exit") {
-        let parts: Vec<&str> = input.split_whitespace().collect();
-        if parts.len() == 2 {
-            if let Ok(code) = parts[1].parse::<i32>() {
-                exit(code);
-            } else {
-                eprintln!("Invalid exit code: {}", parts[1]);
-            }
+pub fn builtin_exit(args: &[String]) {
+    if args.len() == 1 {
+        if let Ok(code) = args[0].parse::<i32>() {
+            exit(code);
         } else {
-            exit(0);
+            eprintln!("Invalid exit code: {}", args[0]);
         }
+    } else {
+        exit(0);
     }
 }
 
-pub fn builtin_echo(input: &str) {
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    if parts.len() > 1 {
-        let message = parts[1..].join(" ");
-        println!("{}", message);
-    } else {
+pub fn builtin_echo(args: &[String]) {
+    if args.is_empty() {
         println!();
+        return;
     }
+    println!("{}", args.join(" "));
 }
 
-pub fn builtin_type(input: &str) {
-    let cmd = &input.split_whitespace().skip(1).next().unwrap_or("");
-    if BUILTINS.contains(&cmd) {
-        println!("{} is a shell builtin", cmd);
+pub fn builtin_type(args: &[String]) {
+    if args.len() == 1 {
+        println!("type requires an argument");
         return;
-    } else {
-        if let Ok(path_var) = env::var("PATH") {
-            for path in env::split_paths(&path_var) {
-                let full_path = Path::new(&path).join(cmd);
-                if full_path.exists() {
-                    println!("{} is {}", cmd, full_path.display());
-                    return;
-                }
+    }
+    let maybe_cmd = &args[1];
+    if BUILTINS.contains(&maybe_cmd.as_str()) {
+        println!("{} is a shell builtin", maybe_cmd);
+        return;
+    }
+    if let Ok(path_var) = env::var("PATH") {
+        for path in env::split_paths(&path_var) {
+            let full_path = Path::new(&path).join(&maybe_cmd);
+            if full_path.exists() {
+                println!("{} is {}", maybe_cmd, full_path.display());
+                return;
             }
         }
     }
-    println!("{}: not found", cmd);
+    println!("{}: not found", maybe_cmd);
 }
 
 pub fn builtin_pwd() {
@@ -54,26 +52,33 @@ pub fn builtin_pwd() {
     }
 }
 
-pub fn builtin_cd(input: &str) {
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    if parts.len() == 1 {
+pub fn builtin_cd(args: &[String]) {
+    if args.len() > 1 {
+        eprintln!("cd: too many arguments");
+        return;
+    }
+
+    //cd with no parameters gets you $HOME
+    if args.len() == 0 {
         if let Ok(home) = env::var("HOME") {
             if let Err(e) = env::set_current_dir(home) {
                 eprintln!("{}", e);
+                return;
+            } else {
+                return;
             }
         }
-    } else if parts.len() == 2 {
-        let path_s = parts[1].replace("~", &env::var("HOME").unwrap_or_default());
-        let path: &Path = Path::new(path_s.as_str());
-
-        if !path.exists() {
-            eprintln!("cd: {}: No such file or directory", parts[1]);
-            return;
-        }
-        if let Err(e) = env::set_current_dir(path) {
-            eprintln!("{}", e);
-        }
-    } else {
-        eprintln!("cd: too many arguments");
     }
+    //Handling ~ as $HOME OR ""
+    let mut maybe_path = args[0].clone();
+    maybe_path = maybe_path.replace("~", &env::var("HOME").unwrap_or_default());
+
+    let path: &Path = Path::new(maybe_path.as_str());
+
+    if !path.exists() {
+        eprintln!("cd: {}: No such file or directory", args[0]);
+        return;
+    }
+
+    env::set_current_dir(path).expect("Can't change work dir");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,84 @@
 use std::io::{self, Write};
 use std::process::ExitStatus;
 mod builtins;
+use std::io::Error;
 
-fn try_exec(input: &str) -> io::Result<ExitStatus> {
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    let command = parts[0];
-    let args = &parts[1..];
-    std::process::Command::new(command).args(args).status()
+fn try_exec(input: &Vec<String>) -> io::Result<ExitStatus> {
+    let maybe_command = &input[0];
+    let args = &input[1..];
+    for path in std::env::var("PATH").unwrap().split(':') {
+        let full_path = std::path::Path::new(path).join(maybe_command);
+        if full_path.exists() {
+            return std::process::Command::new(maybe_command)
+                .args(args)
+                .status();
+        }
+    }
+    Err(Error::new(
+        io::ErrorKind::NotFound,
+        format!("{}: command not found", maybe_command),
+    ))
+}
+
+fn tokenize(input: String) -> Result<Vec<String>, Error> {
+    let mut in_s_quotes: bool = false;
+    let mut out: Vec<String> = vec![];
+    let mut current = String::new();
+    for c in input.chars() {
+        match c {
+            '\'' => in_s_quotes = !in_s_quotes,
+            ' ' if !in_s_quotes => {
+                if current.is_empty() {
+                    continue;
+                }
+                out.push(current.clone());
+                current = String::new();
+            }
+            ' ' if in_s_quotes => {
+                current.push(c);
+            }
+            _ => current.push(c),
+        }
+    }
+    out.push(current.clone());
+    if in_s_quotes {
+        return Err(Error::new(
+            io::ErrorKind::InvalidInput,
+            "Unmatched single quote",
+        ));
+    }
+    // dbg!(&out);
+    Ok(out.clone())
 }
 
 fn main() {
     let stdin = io::stdin();
+
     loop {
         print!("$ ");
         io::stdout().flush().expect("Failed to flush stdout");
+
         let mut input = String::new();
         stdin.read_line(&mut input).expect("Failed to read line");
-        input = input.trim().to_string();
 
-        match input.split_whitespace().next() {
-            Some(command) if builtins::BUILTINS.contains(&command) => match command {
-                "exit" => builtins::builtin_exit(&input),
-                "echo" => builtins::builtin_echo(&input),
-                "type" => builtins::builtin_type(&input),
+        let tokens = tokenize(input.trim().to_string());
+        match tokens {
+            Ok(tokens) => match tokens[0].as_str() {
+                "exit" => builtins::builtin_exit(&tokens[1..]),
+                "echo" => builtins::builtin_echo(&tokens[1..]),
+                "type" => builtins::builtin_type(&tokens),
                 "pwd" => builtins::builtin_pwd(),
-                "cd" => builtins::builtin_cd(&input),
-                _ => println!("{}: command not found", command),
+                "cd" => builtins::builtin_cd(&tokens[1..]),
+                _ => match try_exec(&tokens) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                },
             },
-            _ => match try_exec(&input) {
-                Ok(_) => (),
-                Err(_) => {
-                    println!("{}: command not found", input);
-                }
-            },
+            Err(e) => {
+                println!("{}", e);
+            }
         }
     }
 }
