8633fc39ebc8c28b92ca37702bba53d3f00d49d1
hyper <hyperskill@hyperskill.org>
2025-02-01 02:11:45 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 use std::io::{self, Write};
 use std::process::exit;
 
+fn builtin_exit(input: &str) {
+    if input.starts_with("exit") {
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.len() == 2 {
+            if let Ok(code) = parts[1].parse::<i32>() {
+                exit(code);
+            } else {
+                eprintln!("Invalid exit code: {}", parts[1]);
+            }
+        } else {
+            exit(0);
+        }
+    }
+}
+
+fn builtin_echo(input: &str) {
+    let parts: Vec<&str> = input.split_whitespace().collect();
+    if parts.len() > 1 {
+        let message = parts[1..].join(" ");
+        println!("{}", message);
+    } else {
+        println!();
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
     loop {
@@ -8,21 +33,12 @@ fn main() {
         io::stdout().flush().expect("Failed to flush stdout");
         let mut input = String::new();
         stdin.read_line(&mut input).expect("Failed to read line");
-        let trimmed_input = input.trim();
+        input = input.trim().to_string();
 
-        if trimmed_input.starts_with("exit") {
-            let parts: Vec<&str> = trimmed_input.split_whitespace().collect();
-            if parts.len() == 2 {
-                if let Ok(code) = parts[1].parse::<i32>() {
-                    exit(code);
-                } else {
-                    eprintln!("Invalid exit code: {}", parts[1]);
-                }
-            } else {
-                exit(0);
-            }
+        match input {
+            _ if input.starts_with("exit") => builtin_exit(&input),
+            _ if input.starts_with("echo") => builtin_echo(&input),
+            _ => println!("{}: command not found", input),
         }
-
-        println!("{}: command not found", trimmed_input);
     }
 }
